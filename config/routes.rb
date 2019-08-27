1db720c5d4a77c478a5245b1b6886152161904c1
Joseph Meagalli <joseph@cloudassess.com.au>
2019-08-27 15:22:14 +1000

    removed routes for example

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@
 
 Rails.application.routes.draw do
   root to: 'people#index'
-  match '/people' => 'people#curl__get_example', via: :get
-  match '/people' => 'people#curl__post_example', via: :post
+  # match '/people' => 'people#curl__get_example', via: :get
+  # match '/people' => 'people#curl__post_example', via: :post
   # namespace :api do
   resources :people
   resources :organisations
